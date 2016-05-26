27d462bebe4027bb70dc74aa2741d25bc7f754f6
Ewald Moitzi <ewald.moitzi@gmail.com>
2016-05-26 17:06:36 +0200

    Add simple test for excel_import

--- excel_import/tests.py
+++ excel_import/tests.py
@@ -1,3 +1,17 @@
+import os
 from django.test import TestCase
+from excel_import.models import Document, Row, Cell, DocumentColors
 
-# Create your tests here.
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class ImportTest(TestCase):
+    def simple_import(self):
+        file = os.path.join(BASE_DIR, "excel_import/testdata/test.xlsx")
+        Document.create_document(file)
+
+        self.assertEqual(1, Document.objects.count())
+        self.assertEqual(5, Row.objects.count())
+        self.assertEqual(67, Cell.objects.count())
+        self.assertEqual(3, DocumentColors.objects.count())
