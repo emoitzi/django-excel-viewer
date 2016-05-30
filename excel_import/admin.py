53de70a8d84beb709626d985844022f797a56bf9
Ewald Moitzi <ewald.moitzi@gmail.com>
2016-05-30 20:05:35 +0200

    Add Document and ChangeRequest to admin

--- excel_import/admin.py
+++ excel_import/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from excel_import.models import Document
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'file', 'status', 'created', 'current']
+    list_filter = ['current','created', 'status']
+    search_fields = ['name']
+
+admin.site.register(Document, DocumentAdmin)
--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from frontend.models import ChangeRequest
+
+
+class ChangeRequestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'new_value', 'created_on', 'accepted_on']
+    list_filter = [ 'created_on', 'accepted_on']
+    search_fields = ['author__username', 'author__last_name', 'author__first_name', 'new_value']
+
+
+admin.site.register(ChangeRequest, ChangeRequestAdmin)
