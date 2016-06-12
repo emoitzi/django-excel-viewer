4a8c36ac8acd746fb0bffe391a87821c898d1425
Ewald Moitzi <ewald.moitzi@gmail.com>
2016-06-12 23:14:11 +0200

    Manually add debug toolbar middleware

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,7 +1,9 @@
-import django.contrib.auth.models
+
 
 def show_debug_toolbar(request):
-    if request.user.is_superuser and not request.is_ajax():
+    if request.is_ajax():
+        return False
+    if request.user.is_superuser:
         return True
     if request.get_host() == "127.0.0.1:8000" or \
             request.get_host() == "localhost:8000":
